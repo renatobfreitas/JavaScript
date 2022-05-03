9f729627ea65f3e97c30cf8c78152761cb2b6909
Renato Freitas <103009379+renatobfreitas@users.noreply.github.com>
2022-05-03 12:11:24 -0300

    ex014 .js

---
+++ aula12.ex/ex014/script/script.js
@@ -0,0 +1,24 @@
+function carregar() {
+    var txt = window.document.getElementById('txt')
+    var msg = window.document.getElementById('msg');
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        //bom dia
+        img.src = 'img/manha.png'
+        document.body.style.background = '#f4e0c2'
+        txt.innerHTML = 'Bom dia!'
+    } else if (hora >= 12 && hora < 18) {
+        //boa tarde
+        img.src = 'img/tarde.png'
+        document.body.style.background = '#72444e'
+        txt.innerHTML = 'Boa tarde!'
+    } else {
+        // boa noite
+        img.src = 'img/noite.png'
+        document.body.style.background = '#39393b'
+        txt.innerHTML = 'Boa noite!'
+    }
+}
